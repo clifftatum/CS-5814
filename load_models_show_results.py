3777f35cbcd8f389b2ae1e592e39a3a936ad1ebe
ARL.PSU.EDU\cft5385 <cft5385@psu.edu>
2024-12-03 07:32:57 -0500

    getting repo back up to date

--- load_models_show_results.py
+++ load_models_show_results.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 import os
 import keras
 import tensorflow as tf
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.set_visible_devices([], 'GPU')
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
